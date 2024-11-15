fc951a4a416dc003654ccac7c3244454affd1de6
Masud Rana <masud.rana@apsissolutions.com>
2024-11-15 10:44:25 +0100

    Print linked list soloved

--- src/main/java/org/example/PrintLinkedList.java
+++ src/main/java/org/example/PrintLinkedList.java
@@ -1,2 +1,49 @@
-package org.example;public class PrintLinkedList {
+package org.example;
+
+public class PrintLinkedList {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+    public static void printLinkedList(SinglyLinkedListNode head) {
+
+        SinglyLinkedListNode current = head;
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
